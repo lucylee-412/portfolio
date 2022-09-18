51dc091d59478241bcde6b4af5deda0bdd137d87
Lucy Lee <leelucy412@gmail.com>
2022-09-17 21:14:36 -0400

    Add image preview of resume

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { Link } from "gatsby";
+import { StaticImage } from 'gatsby-plugin-image'
 
 import Layout from "../components/layout"
 // import Seo from "../components/seo"
@@ -7,6 +8,10 @@ import Layout from "../components/layout"
 const ResumePage = () => {
   return (
     <Layout pageTitle="Resume">
+      <StaticImage
+        alt="Preview image of Lucy Lee's resume PDF"
+        src="../images/ResumePreview.jpg"
+      />
       <Link to="/">Go back to the homepage</Link>
     </Layout>
   )
